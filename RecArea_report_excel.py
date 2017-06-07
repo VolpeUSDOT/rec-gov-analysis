f0a47652aac200b4225bc05b62211ae40807ae08
Matt Goodwin <Matthew.Goodwin@dot.gov>
2017-06-07 15:42:51 -0400

    Added Entity/Location Breakdown
    Added Entity Type and Location Breakdown tables to RecArea Workbook. Need to figure out how to handle ZipCodes at this level (but maybe we just keep it at state and let user choose to dig deeper into facilities)

--- RecArea_report_excel.py
+++ RecArea_report_excel.py
@@ -64,6 +64,120 @@ for recarea in RecAreas:
     target_fac = pd.read_sql_query(temp_fac_target_query, recreation_cnxn)
     target_fac = target_fac.reset_index()
     
+    #Run Analysis on collected facility data for RecArea
+    
+    #Start with pandas based sheets as those are easier to implement
+    print (datetime.datetime.now().time())
+    #Set up workbook
+    new_file = os.path.join(new_folder, recarea + '.xls')
+    wb = xlwt.Workbook()
+     
+    
+    #Item 1: In-state/out-of-state/intl distinction
+    print ("Customer Origin Analysis")
+    #Count Countries where reservations come from and convert to dataframe
+    country_count = target_fac['CustomerCountry'].value_counts().to_frame().reset_index()
+    
+    #Setup sheet where this and the other relevant info will go
+    custloc_sheet = wb.add_sheet("Customer Location Breakdown")
+    #custloc_sheet.write()
+    custloc_sheet.write(0,0,"Reservation Breakdown by Country")
+    custloc_sheet.write(1,0,"Country")
+    custloc_sheet.write(1,1,"# of Reservations")
+    for index, row in country_count.iterrows():
+        custloc_sheet.write(int(index)+2,0,row['index'])
+        custloc_sheet.write(int(index)+2,1,row['CustomerCountry'])
+   
+    #In State/Out of State/Out of Country distinction
+    
+    #Total site reservaations calcualtion
+    total_res=len(target_fac)
+    
+    #Collect reservations made by residents of the faciliity's state
+    instate_res=len(target_fac.loc[target_fac['CustomerState']==target_fac['FacilityState']])
+    #outcountry_res =target_fac.loc[target_fac['CustomerState']!=target_fac['FacilityState'] & target_fac['CustomerCountry']='USA']
+    
+    #Collect reservations made by non-USA residents
+    outcountry_res =len(target_fac.loc[target_fac['CustomerCountry']!='USA'])
+    
+    #Calculate residents that are out of state
+    ##Total Reservations-(instate_res+outcountrye_res)=out of state residents
+    outstate_res = total_res-(instate_res+outcountry_res)
+    
+    # Write this results to Customer Location Breakdown Sheet
+    custloc_sheet.write(0,4,"Reservation Breakdown by State")
+    custloc_sheet.write(1,4,"Category")
+    custloc_sheet.write(1,5,"# of Reservations")
+    custloc_sheet.write(2,4,"Same State as Site")
+    custloc_sheet.write(2,5,instate_res)
+    custloc_sheet.write(3,4,"Out of State")
+    custloc_sheet.write(3,5,outstate_res)
+    custloc_sheet.write(4,4,"Outside USA")
+    custloc_sheet.write(4,5,outcountry_res)
+    custloc_sheet.write(5,4,"Total Reservations")
+    custloc_sheet.write(5,5,total_res)
+    
+    wb.save(new_file)
+    
+    
+    
+     #############################################################
+    #Item 3 Zip code local/non-local distinction Note: Some Facilities do not have Zip
+    
+    #Level 1: Reservations has same zip code as site
+    local_res_lev1 = len(target_fac.loc[target_fac['CustomerZIP']==target_fac['FacilityZIP']])
+    #Level 2: Reservations have same 3 digit level zip as facility
+    #Pull facility ZipCode (just use first row data as this should remanin the same for the filtered sheet)
+    
+    #set level of zip code to check i.e zip_lvl=3 for 33027 would check against 330*
+    zip_lvl = 3
+    fac_zip = target_fac['FacilityZIP'].iloc[0][:zip_lvl]
+    #create new columns with ZipCodes as strings to use regex with
+    target_fac['CustomerZIP_Str']=target_fac['CustomerZIP']
+    target_fac['CustomerZIP_Str']=target_fac['CustomerZIP_Str'].apply(str)
+    #form 3 digit regex expression. if handles if there is no Zip
+    print ("Zip Codes Local/NonLocal Analysis")
+    
+    
+    if fac_zip != '':
+        fac_zip_regex=fac_zip+'*'
+        local_res_lev2=len(target_fac['CustomerZIP'].filter(regex=fac_zip_regex))
+        #write out to Breakdown sheet if data exists
+        custloc_sheet.write(0,7,"Reservation Breakdown by Zip Code")
+        custloc_sheet.write(1,7,"Category")
+        custloc_sheet.write(1,8,"# of Reservations")
+        custloc_sheet.write(2,7,"Same Zip as Site")
+        custloc_sheet.write(2,8,local_res_lev1)
+        custloc_sheet.write(3,7,"Within same "+str(zip_lvl)+ " Digit Level as Site")
+        custloc_sheet.write(3,8,local_res_lev2)
+        custloc_sheet.write(4,7,"Total Reservations")
+        custloc_sheet.write(4,8,total_res)
+
+    else:
+        print('No Facility Zip Code Available in Data Set')
+    
+    #############################################################
+    #Item 1 - Add entity type to standard report
+    #get entity counts as a data frame to iterate over
+    entity_count = target_fac['EntityType'].value_counts().to_frame().reset_index()
+    #print (len(entity_count))
+    #write to new sheet
+    
+    # Entity Type
+    print ("Entity Type")
+    
+    ent_sheet = wb.add_sheet("EntityType")
+    ent_sheet.write(0,0,'Entity Type')
+    ent_sheet.write(0,1,'# of Reservations')
+    for index, row in entity_count.iterrows():
+        ent_sheet.write(int(index)+1,0,row['index'])
+        ent_sheet.write(int(index)+1,1,row['EntityType'])
+   
+        
+
+    
+    wb.save(new_file)
+    
     
 #Close db  connections
 recreation_cursor.close()
