40b59e13e35fe25195f0d76fc58626199a627d1b
Matt Goodwin <Matthew.Goodwin@dot.gov>
2017-06-12 14:53:58 -0400

    Minor cleanup
    Remove some debugging print statements

--- RecArea_report_excel.py
+++ RecArea_report_excel.py
@@ -300,7 +300,6 @@ for recarea in RecAreas:
     i = 0
     for row in res_year.iterrows():
         date = row[0].strftime('%m-%d-%Y')
-        print (date)
         rec_dates.write(i+1,0,str(date))
         rec_dates.write(i+1,1,int(res_year.loc[row[0]]['Reservations']))
        
