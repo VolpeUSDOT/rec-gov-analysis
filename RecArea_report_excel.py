6bc28337c34e7c10f73c00a3faa1cf942bb06f59
Matt Goodwin <Matthew.Goodwin@dot.gov>
2017-06-20 09:25:06 -0400

    Commenting
    Added some clarifying comments

--- RecArea_report_excel.py
+++ RecArea_report_excel.py
@@ -35,8 +35,8 @@ RecAreas = ['25'] #['10',17','25','122']
 
 #Adjust YEARS list for each year you want analysis for
 #YEAR_TABLE will be automatically updated to have the Table names for the necessary sheets based on YEARS
-#YEARS = [2015,2014] #All years [2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006]
-YEARS = [2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006]
+YEARS = [2015,2014] #All years [2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006]
+#YEARS = [2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006]
 
 #No need to modify once YEARS is set
 YEAR_TABLE = []
